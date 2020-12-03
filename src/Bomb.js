bf9966e753413e174aaeb9046d370e30c1a5ca1b
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-12-02 21:10:09 -0500

    Done

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,32 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    response = (stateValue) => {
+        if (stateValue >= 1){
+            return(
+                <div>
+                {stateValue} seconds left before I go boom!
+                </div>
+            )
+        } else {
+            return(
+                <div>
+                    Boom!
+                </div>
+            )
+        }
+    }
+    render() {
+        return(
+        this.response(this.state.secondsLeft)
+        )
+      }
+        
+}
